615ca8f8d2534fedf1a36d40f6bd39658df5c7b4
Michael Cadigan <mikejcadigan@gmail.com>
2018-10-09 22:10:25 -0400

    no message

--- lib/src/f2f_notifications.dart
+++ lib/src/f2f_notifications.dart
@@ -1,204 +1,218 @@
-import 'package:f2f_notifications/f2f_notifications.dart';
-import 'package:f2fservices/f2fservices.dart';
-import 'package:flutter/material.dart';
-import 'dart:math';
-import 'package:face2face/components/reminders/reminderModel.dart';
-import 'package:face2face/main.dart' as main;
-class ReminderHelper {
-  // final String interval;
-  final _random = new Random();
-  final bool doNotRequestNotifications;
-  ReminderHelper({this.doNotRequestNotifications});
-  NotificationDetails platformChannelSpecifics = main.platformChannelSpecifics;
-  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = main.flutterLocalNotificationsPlugin;
-  BuildContext context;
-  RepeatInterval repeatInterval;
-  Expression expression;
-  RepeatInterval getInterval(String interval) {
-    switch (interval) {
-      case 'Never':
-        return null;
-      case 'Every week':
-        return RepeatInterval.Weekly;
-      case 'Every other week':
-        return RepeatInterval.WeeklyTwo;
-      case 'Every three weeks':
-        return RepeatInterval.WeeklyThree;
-      case 'Every four weeks':
-        return RepeatInterval.WeeklyFour;
-      default:
-        return null;
+part of f2f_notifications;
+
+typedef Future<dynamic> MessageHandler(String message);
+
+/// The available intervals for periodically showing notifications
+enum RepeatInterval { Weekly, WeeklyTwo, WeeklyThree, WeeklyFour }
+
+/// The days of the week
+class Day {
+  static const Monday = const Day(2);
+  static const Tuesday = const Day(3);
+  static const Wednesday = const Day(4);
+  static const Thursday = const Day(5);
+  static const Friday = const Day(6);
+  static const Saturday = const Day(7);
+  static const Sunday = const Day(1);
+
+  static get values =>
+      [Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday];
+
+  final int value;
+
+  const Day(this.value);
+}
+
+/// Used for specifying a time in 24 hour format
+class Time {
+  /// The hour component of the time. Accepted range is 0 to 23 inclusive
+  final int hour;
+
+  /// The minutes component of the time. Accepted range is 0 to 59 inclusive
+  final int minute;
+
+  /// The seconds component of the time. Accepted range is 0 to 59 inclusive
+  final int second;
+
+  Time([this.hour = 0, this.minute = 0, this.second = 0]) {
+    assert(this.hour >= 0 && this.hour < 24);
+    assert(this.minute >= 0 && this.minute < 60);
+    assert(this.second >= 0 && this.second < 60);
+  }
+
+  Map<String, int> toMap() {
+    return <String, int>{
+      'hour': hour,
+      'minute': minute,
+      'second': second,
+    };
+  }
+}
+
+class FlutterLocalNotificationsPlugin {
+  factory FlutterLocalNotificationsPlugin() => _instance;
+
+  @visibleForTesting
+  FlutterLocalNotificationsPlugin.private(
+      MethodChannel channel, Platform platform)
+      : _channel = channel,
+        _platform = platform;
+
+  static final FlutterLocalNotificationsPlugin _instance =
+      new FlutterLocalNotificationsPlugin.private(
+          const MethodChannel('dexterous.com/flutter/local_notifications'),
+          const LocalPlatform());
+
+  final MethodChannel _channel;
+  final Platform _platform;
+
+  MessageHandler onSelectNotification;
+
+  /// Initializes the plugin. Call this method on application before using the plugin further
+  Future<bool> initialize(InitializationSettings initializationSettings,
+      {MessageHandler selectNotification}) async {
+    onSelectNotification = selectNotification;
+    var serializedPlatformSpecifics =
+        _retrievePlatformSpecificInitializationSettings(initializationSettings);
+    _channel.setMethodCallHandler(_handleMethod);
+    var result =
+        await _channel.invokeMethod('initialize', serializedPlatformSpecifics);
+    return result;
+  }
+
+  Future<NotificationAppLaunchDetails> getNotificationAppLaunchDetails() async {
+    var result = await _channel.invokeMethod('getNotificationAppLaunchDetails');
+    return NotificationAppLaunchDetails(result['notificationLaunchedApp'],
+        result.containsKey('payload') ? result['payload'] : null);
+  }
+
+  /// Show a notification with an optional payload that will be passed back to the app when a notification is tapped
+  Future show(int id, String title, String body,
+      NotificationDetails notificationDetails,
+      {String payload}) async {
+    var serializedPlatformSpecifics =
+        _retrievePlatformSpecificNotificationDetails(notificationDetails);
+    await _channel.invokeMethod('show', <String, dynamic>{
+      'id': id,
+      'title': title,
+      'body': body,
+      'platformSpecifics': serializedPlatformSpecifics,
+      'payload': payload ?? ''
+    });
+  }
+
+  /// Cancel/remove the notification with the specified id. This applies to notifications that have been scheduled and those that have already been presented.
+  Future cancel(int id) async {
+    await _channel.invokeMethod('cancel', id);
+  }
+
+  /// Cancels/removes all notifications. This applies to notifications that have been scheduled and those that have already been presented.
+  Future cancelAll() async {
+    await _channel.invokeMethod('cancelAll');
+  }
+
+  /// Schedules a notification to be shown at the specified time with an optional payload that is passed through when a notification is tapped
+  Future schedule(int id, String title, String body, DateTime scheduledDate,
+      NotificationDetails notificationDetails,
+      {String payload}) async {
+    var serializedPlatformSpecifics =
+        _retrievePlatformSpecificNotificationDetails(notificationDetails);
+    await _channel.invokeMethod('schedule', <String, dynamic>{
+      'id': id,
+      'title': title,
+      'body': body,
+      'millisecondsSinceEpoch': scheduledDate.millisecondsSinceEpoch,
+      'platformSpecifics': serializedPlatformSpecifics,
+      'payload': payload ?? ''
+    });
+  }
+
+  /// Periodically show a notification using the specified interval.
+  /// For example, specifying a hourly interval means the first time the notification will be an hour after the method has been called and then every hour after that.
+  Future periodicallyShow(int id, String title, String body,
+      RepeatInterval repeatInterval, NotificationDetails notificationDetails,
+      {String payload}) async {
+    var serializedPlatformSpecifics =
+        _retrievePlatformSpecificNotificationDetails(notificationDetails);
+    await _channel.invokeMethod('periodicallyShow', <String, dynamic>{
+      'id': id,
+      'title': title,
+      'body': body,
+      'calledAt': new DateTime.now().millisecondsSinceEpoch,
+      'repeatInterval': repeatInterval.index,
+      'platformSpecifics': serializedPlatformSpecifics,
+      'payload': payload ?? ''
+    });
+  }
+
+  /// Shows a notification on a daily interval at the specified time
+  // Future showDailyAtTime(int id, String title, String body,
+  //     Time notificationTime, NotificationDetails notificationDetails,
+  //     {String payload}) async {
+  //   var serializedPlatformSpecifics =
+  //       _retrievePlatformSpecificNotificationDetails(notificationDetails);
+  //   await _channel.invokeMethod('showDailyAtTime', <String, dynamic>{
+  //     'id': id,
+  //     'title': title,
+  //     'body': body,
+  //     'calledAt': new DateTime.now().millisecondsSinceEpoch,
+  //     'repeatInterval': RepeatInterval.Weekly.index,
+  //     'repeatTime': notificationTime.toMap(),
+  //     'platformSpecifics': serializedPlatformSpecifics,
+  //     'payload': payload ?? ''
+  //   });
+  // }
+
+  /// Shows a notification on a daily interval at the specified time and interval
+  Future scheduleFacetoFaceNotification(
+      int id,
+      String title,
+      String body,
+      Day day,
+      Time notificationTime,
+      RepeatInterval interval,
+      DateTime scheduledDate,
+      NotificationDetails notificationDetails,
+      {String payload}) async {
+    var serializedPlatformSpecifics =
+        _retrievePlatformSpecificNotificationDetails(notificationDetails);
+    await _channel.invokeMethod('showWeeklyAtDayAndTime', <String, dynamic>{
+      'id': id,
+      'title': title,
+      'body': body,
+      'calledAt': new DateTime.now().millisecondsSinceEpoch,
+      'repeatInterval': interval.index,
+      'repeatTime': notificationTime.toMap(),
+      'day': day.value,
+      'millisecondsSinceEpoch': scheduledDate.millisecondsSinceEpoch,
+      'platformSpecifics': serializedPlatformSpecifics,
+      'payload': payload ?? ''
+    });
+  }
+
+  Map<String, dynamic> _retrievePlatformSpecificNotificationDetails(
+      NotificationDetails notificationDetails) {
+    Map<String, dynamic> serializedPlatformSpecifics;
+    if (_platform.isAndroid) {
+      serializedPlatformSpecifics = notificationDetails?.android?.toMap();
+    } else if (_platform.isIOS) {
+      serializedPlatformSpecifics = notificationDetails?.iOS?.toMap();
     }
+    return serializedPlatformSpecifics;
   }
 
-  deleteReminder(ReminderModel reminder) async {
-    print('Now trying to delete notification in system with id: ' +
-        reminder.id.toString());
-    await flutterLocalNotificationsPlugin.cancel(reminder.id);
-    await ReminderModel().deleteReminder(reminder);
-  }
-
-  String getDayString(int id) {
-    switch (id) {
-      case 1:
-        return 'Monday';
-      case 2:
-        return 'Tuesday';
-      case 3:
-        return 'Wednesday';
-      case 4:
-        return 'Thursday';
-      case 5:
-        return 'Friday';
-      case 6:
-        return 'Saturday';
-      case 7:
-        return 'Sunday';
-      default:
-        return 'Sunday';
+  Map<String, dynamic> _retrievePlatformSpecificInitializationSettings(
+      InitializationSettings initializationSettings) {
+    Map<String, dynamic> serializedPlatformSpecifics;
+    if (_platform.isAndroid) {
+      serializedPlatformSpecifics = initializationSettings?.android?.toMap();
+    } else if (_platform.isIOS) {
+      serializedPlatformSpecifics = initializationSettings?.ios?.toMap();
     }
-  }
-  String getMonthString(int id) {
-    switch (id) {
-      case 1:
-        return 'Jan';
-      case 2:
-        return 'Feb';
-      case 3:
-        return 'Mar';
-      case 4:
-        return 'Apr';
-      case 5:
-        return 'May';
-      case 6:
-        return 'Jun';
-      case 7:
-        return 'Jul';
-      case 8:
-        return 'Aug';
-      case 9:
-        return 'Sep';
-      case 10:
-        return 'Oct';
-      case 11:
-        return 'Nov';
-      case 12:
-        return 'Dec';
-      default:
-        return 'Jan';
-    }
-  }
-  Day convertToDay(int id) {
-    switch (id) {
-      case 1:
-        return Day.Monday;
-      case 2:
-        return Day.Tuesday;
-      case 3:
-        return Day.Wednesday;
-      case 4:
-        return Day.Thursday;
-      case 5:
-        return Day.Friday;
-      case 6:
-        return Day.Saturday;
-      case 7:
-        return Day.Sunday;
-      default:
-        return Day.Sunday;
-    }
+    return serializedPlatformSpecifics;
   }
 
-  scheduleReminder(String interval, int id, Time time, DateTime selectedDate,
-      Expression expression, BuildContext context) async {
-    
-    this.context = context;
-    this.expression = expression;
-    RepeatInterval repeatInterval = getInterval(interval);
-    if (repeatInterval == null) {}
-
-    //var reminders = await ReminderModel().getReminders();
-    //  var idsAlreadyUsed = List<int>();
-    //  for (ReminderModel reminder in reminders) {
-    //    idsAlreadyUsed.add(reminder.id);
-    //  }
-
-    id = next(0, 999999);
-    
-    print('Scheduling with ' +
-        interval +
-        ', Id: ' +
-        id.toString() +
-        ', time: ' +
-        time.toString() +
-        ' date: ' +
-        selectedDate.toString() +
-        ' exercise: ' +
-        expression.title +
-        (selectedDate.weekday).toString());
-
-    selectedDate = new DateTime(selectedDate.year, selectedDate.month,
-        selectedDate.day, time.hour, time.minute);
-
-    if (repeatInterval == null) {
-      print('Once');
-     print('Current second' + DateTime.now().toString());
-      print('Will go off on' + selectedDate.toString());
-      var reminder = ReminderModel(
-          categoryTitle: expression.type,
-          weekDay: getDayString(selectedDate.weekday),
-          id: id,
-          dateTimeString: selectedDate.toString(),
-          repeat: interval.toString(),
-          title: expression.title);
-      reminder.save(reminder);
-      
-      //  if (selectedDate.isBefore(DateTime.now())) {
-      //   return;
-      // } 
-      //once off
-      await flutterLocalNotificationsPlugin.schedule(
-          id,
-          expression.title,
-          'It is time to do your exercise.',
-          selectedDate,
-          platformChannelSpecifics,
-          payload: expression.id.toString());
-
-    
-      return;
-    }
-// selectedDate = selectedDate.add(new Duration(seconds: 10));
-    //if repeating
-    print('Repeat');
- //   print('Current second ' +
-    //    DateTime.now().minute.toString() +
-     //   DateTime.now().second.toString());
- //   print('Will go off on ' + selectedDate.minute.toString() + selectedDate.second.toString());
-    
-    Day day = convertToDay(selectedDate.weekday);
-    print(day.value);
-    await flutterLocalNotificationsPlugin.scheduleFacetoFaceNotification(
-        id,
-        expression.title,
-        'It is time to do your exercise.',
-        convertToDay(selectedDate.weekday),
-        Time(selectedDate.hour, selectedDate.minute),
-        repeatInterval,
-        selectedDate,
-        platformChannelSpecifics,
-        payload: expression.id.toString());
-
-    var reminder = ReminderModel(
-        categoryTitle: expression.type,
-        weekDay: getDayString(selectedDate.weekday),
-        id: id,
-        dateTimeString: selectedDate.toString(),
-        repeat: interval.toString(),
-        title: expression.title);
-    reminder.save(reminder);
+  Future _handleMethod(MethodCall call) {
+    return onSelectNotification(call.arguments);
   }
-
-  int next(int min, int max) => min + _random.nextInt(max - min);
-
 }
